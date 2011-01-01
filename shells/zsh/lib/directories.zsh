6e424ce5506082ba2f550f2b6057447865883200
jpablobr <xjpablobrx@gmail.com>
2011-01-01 19:50:25 +1000

    aliases updates for cd on zsh shell

--- shells/zsh/lib/directories.zsh
+++ shells/zsh/lib/directories.zsh
@@ -3,13 +3,6 @@ setopt auto_name_dirs
 setopt auto_pushd
 setopt pushd_ignore_dups
 
-alias ..='cd ..'
-alias cd..='cd ..'
-alias cd...='cd ../..'
-alias cd....='cd ../../..'
-alias cd.....='cd ../../../..'
-alias cd/='cd /'
-
 alias 1='cd -'
 alias 2='cd +2'
 alias 3='cd +3'
@@ -20,6 +13,7 @@ alias 7='cd +7'
 alias 8='cd +8'
 alias 9='cd +9'
 
+alias ..='cd ..'
 cd () {
   if   [[ "x$*" == "x..." ]]; then
     cd ../..
