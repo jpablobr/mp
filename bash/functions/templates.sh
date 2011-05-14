e05f3e95c1bab49379d966c652401f41f262d709
jpablobr <xjpablobrx@gmail.com>
2011-05-14 22:14:36 +1000

    added rails db.yml template

--- bash/functions/templates.sh
+++ bash/functions/templates.sh
@@ -4,10 +4,35 @@
 # Author: José Pablo Barrantes R. <xjpablobrx@gmail.com>
 
 t-rvmrc() {
-  test $# != 1 && echo "Please pass a name for the gemset!" && return 1
+  test $# != 1 && echo "Please provide a name for the gemset!" && return 1
 
   cat > .rvmrc << -EOF-
 rvm use 1.9.2-p180@$1 --create
+-EOF-
+  return 0
+}
+
+t-database-yml() {
+  test $# != 1 && echo "Please provide a name for the database.yml file!" && return 1
+
+  cat > database.yml << -EOF-
+defaults: &defaults
+  adapter: postgresql
+  encoding: unicode
+  database: $1_development
+  pool: 5
+  host: 127.0.0.1
+
+development:
+  <<: *defaults
+
+test:
+  <<: *defaults
+  database: $1_test
+
+production:
+  <<: *defaults
+  database: $1_production
 -EOF-
   return 0
 }
