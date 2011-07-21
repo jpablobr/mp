3614adcf6bf49856dd886faf151e2469bf78e3e9
jpablobr <xjpablobrx@gmail.com>
2011-07-21 11:36:22 +1000

    updated awesome_print in irbrc.d

--- dotfiles/.irbrc.d/awesome_print.rb
+++ dotfiles/.irbrc.d/awesome_print.rb
@@ -1,10 +1,18 @@
 # Installing as Ruby gem
 # $ gem install awesome_print
-require 'ap'
+require "rubygems"
+require "awesome_print"
 
-IRB::Irb.class_eval do
-  def output_value
-    ap @context.last_value
+unless IRB.version.include?('DietRB')
+  IRB::Irb.class_eval do
+    def output_value
+      ap @context.last_value
+    end
   end
+else # MacRuby
+  IRB.formatter = Class.new(IRB::Formatter) do
+    def inspect_object(object)
+      object.ai
+    end
+  end.new
 end
-
