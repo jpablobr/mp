14d9f2bf50327aebf017773322d8a07b74822579
jpablobr <xjpablobrx@gmail.com>
2011-08-08 01:31:21 +1000

    renamed jp_startups

--- bin/jp_startups.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gnome-terminal --hide-menubar --full-screen -e '/bin/bash /home/jpablobr/.tmuxinator/jpablobr.tmux'
---
+++ bin/jpablobr-startups.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xfce4-terminal --hide-menubar --maximize -e '/bin/bash /home/jpablobr/.tmuxinator/jpablobr.tmux'
