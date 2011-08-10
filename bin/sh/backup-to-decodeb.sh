75d6e5ba8a96e22720a22edba5961f75f0072987
jpablobr <xjpablobrx@gmail.com>
2011-08-10 13:54:25 +1000

    removing stuff

--- bin/sh/backup-to-decodeb.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-rsync -e ssh -auzvr --delete --exclude-from \
-    $HOME/.rsync-exclude /home/jpablobr/ \
-    decodeb:/media/toshiba-1t/rsync-jpablobr/
--- bin/sh/backup-to-toshiba.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-rsync -auzvr --delete --exclude-from \
-    $HOME/.rsync-exclude /home/jpablobr/ \
-    /media/toshiba-1t/rsync-jpablobr/
-
