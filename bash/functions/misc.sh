a58b24f6f2a167f0d41af053952b47e202b1f1ff
jpablobr <xjpablobrx@gmail.com>
2011-12-11 19:10:55 +1000

    finally kind of happy with the xesources theme

--- bash/functions/misc.sh
+++ bash/functions/misc.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-# misc.sh
-# General helpers
-# Author: José Pablo Barrantes R. <xjpablobrx@gmail.com>
 
 alias pygrep="grep --include='*.py' $*"
 alias rbgrep="grep --include='*.rb' $*"
@@ -11,9 +8,6 @@ alias chmodfx="find . -type f -print0 | xargs -0 chmod +x"
 alias scan="sudo iwlist wlan0 scan | grep ESSID"
 alias scanfull="sudo iwlist wlan0 scan"
 alias grep="grep --color=auto"
-alias lla="ls -la"
-alias la="ls -a"
-alias llatr="ls -latr"
 alias ps.cpu="ps ahuwx | sort -nr -k 3 | head -n10"
 alias ps.mem="ps ahuwx | sort -nr -k 4 | head -n10"
 # multiple-spaces > single-space
@@ -148,87 +142,14 @@ mkcd() {
   cd "$*"
 }
 
-duh() { # disk usage for humans
-  test $# -eq 0 && set -- *
-  du -sch "$@" | sort -h
+debug_http () {
+    /usr/bin/curl    \
+        $@           \
+        -o /dev/null \
+        -w "dns: %{time_namelookup} connect: %{time_connect} pretransfer: %{time_pretransfer} starttransfer: %{time_starttransfer} total: %{time_total}\n"
 }
 
-ac() { # compress a file or folder
-    case "$1" in
-           tar.bz2|.tar.bz2) tar cvjf "${2%%/}.tar.bz2" "${2%%/}/"  ;;
-       tbz2|.tbz2)       tar cvjf "${2%%/}.tbz2" "${2%%/}/"     ;;
-       tbz|.tbz)         tar cvjf "${2%%/}.tbz" "${2%%/}/"      ;;
-       tar.xz)         tar cvJf "${2%%/}.tar.gz" "${2%%/}/"      ;;
-       tar.gz|.tar.gz)   tar cvzf "${2%%/}.tar.gz" "${2%%/}/"   ;;
-       tgz|.tgz)         tar cvjf "${2%%/}.tgz" "${2%%/}/"      ;;
-       tar|.tar)         tar cvf  "${2%%/}.tar" "${2%%/}/"        ;;
-           rar|.rar)         rar a "${2}.rar" "$2"            ;;
-       zip|.zip)         zip -9 "${2}.zip" "$2"            ;;
-       7z|.7z)         7z a "${2}.7z" "$2"            ;;
-       lzo|.lzo)         lzop -v "$2"                ;;
-       gz|.gz)         gzip -v "$2"                ;;
-       bz2|.bz2)         bzip2 -v "$2"                ;;
-       xz|.xz)         xz -v "$2"                    ;;
-       lzma|.lzma)         lzma -v "$2"                ;;
-           *)           echo "ac(): compress a file or directory."
-            echo "Usage:   ac <archive type> <filename>"
-                echo "Example: ac tar.bz2 PKGBUILD"
-            echo "Please specify archive type and source."
-            echo "Valid archive types are:"
-            echo "tar.bz2, tar.gz, tar.gz, tar, bz2, gz, tbz2, tbz,"
-            echo "tgz, lzo, rar, zip, 7z, xz and lzma." ;;
-    esac
-}
-ad() { # decompress archive (to directory $2 if wished for and possible)
-   if [ -f "$1" ] ; then
-       case "$1" in
-           *.tar.bz2|*.tgz|*.tbz2|*.tbz) mkdir -v "$2" 2>/dev/null ; tar xvjf "$1" -C "$2" ;;
-       *.tar.gz)             mkdir -v "$2" 2>/dev/null ; tar xvzf "$1" -C "$2" ;;
-       *.tar.xz)             mkdir -v "$2" 2>/dev/null ; tar xvJf "$1" ;;
-       *.tar)             mkdir -v "$2" 2>/dev/null ; tar xvf "$1"  -C "$2" ;;
-       *.rar)             mkdir -v "$2" 2>/dev/null ; 7z x   "$1"     "$2" ;;
-       *.zip)             mkdir -v "$2" 2>/dev/null ; unzip   "$1"  -d "$2" ;;
-       *.7z)             mkdir -v "$2" 2>/dev/null ; 7z x    "$1"   -o"$2" ;;
-       *.lzo)             mkdir -v "$2" 2>/dev/null ; lzop -d "$1"   -p"$2" ;;
-       *.gz)             gunzip "$1"                       ;;
-       *.bz2)             bunzip2 "$1"                       ;;
-       *.Z)                 uncompress "$1"                       ;;
-       *.xz|*.txz|*.lzma|*.tlz)     xz -d "$1"                           ;;
-       *)
-       esac
-   else
-                      echo "Sorry, '$2' could not be decompressed."
-              echo "Usage: ad <archive> <destination>"
-              echo "Example: ad PKGBUILD.tar.bz2 ."
-              echo "Valid archive types are:"
-              echo "tar.bz2, tar.gz, tar.xz, tar, bz2,"
-              echo "gz, tbz2, tbz, tgz, lzo,"
-              echo "rar, zip, 7z, xz and lzma"
-   fi
-}
-
-al() { # list content of archive but don't unpack
-    if [ -f "$1" ]; then
-         case "$1" in
-       *.tar.bz2|*.tbz2|*.tbz) tar -jtf "$1"     ;;
-       *.tar.gz)                     tar -ztf "$1"     ;;
-       *.tar|*.tgz|*.tar.xz)                 tar -tf "$1"     ;;
-       *.gz)                 gzip -l "$1"     ;;
-       *.rar)                 rar vb "$1"     ;;
-       *.zip)                 unzip -l "$1"     ;;
-       *.7z)                 7z l "$1"     ;;
-       *.lzo)                 lzop -l "$1"     ;;
-       *.xz|*.txz|*.lzma|*.tlz)      xz -l "$1"     ;;
-         esac
-    else
-         echo "Sorry, '$1' is not a valid archive."
-     echo "Valid archive types are:"
-     echo "tar.bz2, tar.gz, tar.xz, tar, gz,"
-     echo "tbz2, tbz, tgz, lzo, rar"
-     echo "zip, 7z, xz and lzma"
-    fi
-}
-
-full_path() {
-    echo "$(cd $(dirname "$1"); pwd -P)/$(basename "$1")"
+# http_headers: get just the HTTP headers from a web page (and its redirects)
+http_headers () {
+    /usr/bin/curl -I -L $@
 }
